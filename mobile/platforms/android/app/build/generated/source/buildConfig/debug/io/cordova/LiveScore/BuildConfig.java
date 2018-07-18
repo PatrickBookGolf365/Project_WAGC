332137384b98c50fb758d2005bd31dbc332db523
Patrick <Patrick@Bookgolf365.com>
2018-07-18 15:42:34 +0100

    Updated App Icon

--- mobile/platforms/android/app/build/generated/source/buildConfig/debug/io/cordova/LiveScore/BuildConfig.java
+++ mobile/platforms/android/app/build/generated/source/buildConfig/debug/io/cordova/LiveScore/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.cordova.LiveScore";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 800000;
+  public static final int VERSION_CODE = 900000;
   public static final String VERSION_NAME = "4.0.0";
 }
